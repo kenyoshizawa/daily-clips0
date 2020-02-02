066d08277b63ba76014b7525bb22e446457106d7
kenyoshizawa <harborseal2521@gmail.com>
2020-02-02 11:08:21 +0900

    create user

---
+++ config/initializers/generators.rb
@@ -0,0 +1,5 @@
+Rails.application.config.generators do |g|
+  g.stylesheets false
+  g.helper false
+  g.skip_routes true
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'users#index'
+  get 'signup', to: 'users#new'
+
+  resources :users, only: [:index, :show, :new, :create]
+  #   get 'users', to: 'users#index'
+  #   post 'users', to: 'users#create'
+  #   get 'users/new',  to:'users#new'
+  #   get 'user/:id', to: 'users#show'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
