c2e5b13d501543bf2aea86aaa13bd571280981f8
kenyoshizawa <harborseal2521@gmail.com>
2020-03-14 01:48:57 +0900

    add image

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,7 @@ class UsersController < ApplicationController
   end
 
   def create
-
     @user = User.new(user_params)
-    binding.pry
-
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
       redirect_to(login_url)
